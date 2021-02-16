e9d76ed55a6ff0bd2fa969dbf161f30faf2d7b9f
Anna Melnichuk <varchuk.anna.94@gmail.com>
2021-02-16 11:34:41 +0200

    Created toggled menu

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+let menuElement = document.querySelector('.menu-icon');
+menuElement.onclick = function() {
+    const element = document.querySelector('.navigation');
+    if(element.style.display == 'flex')
+    element.style.display = 'none';
+ else
+    element.style.display = 'flex';
+}
+
